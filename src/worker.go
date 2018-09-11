7bae6a19c0c0117a2a9dcbe4678cfb15023098fb
huripklb <huripto.sugandi@kalbenutritionals.com>
2018-09-11 23:14:56 +0700

    Use global db connection, add clear data

--- src/worker.go
+++ src/worker.go
@@ -4,22 +4,31 @@ package main
         _ "github.com/go-sql-driver/mysql"
         "database/sql"
         "fmt"
-        //"strings"
         "time"
     )
 
     func main() {
-    	//ch := make(chan int, 4)
-        //go getOrderAge("pending", ch)
-        //go getOrderAge("proceed", ch)
-        //go getOrderAge("shipped", ch)
-        //go getOrderAge("delivered", ch)
-        go getOrderAge("pending")
-        go getOrderAge("proceed")
-        go getOrderAge("shipped")
-        go getOrderAge("delivered")
+    	db, err := sql.Open("mysql", "root:huripdb@/newkalbe?charset=utf8")
+    	checkErr(err)
 
-        time.Sleep(2 * time.Second)
+    	// clear the table
+    	clear(db)
+    	// don't use go routine. this process must finish first
+
+    	// get orders age
+        go getOrderAge(db, "pending")
+        go getOrderAge(db, "proceed")
+        go getOrderAge(db, "shipped")
+        go getOrderAge(db, "delivered")
+
+        // get PO age
+        go getPoAge(db, "draft")
+        go getPoAge(db, "shipped")
+        //go getPoAge(db, "completed")
+
+        // make sure the channel doesn't close while executing go routine
+        // other method available, will be done in next research
+        time.Sleep(3 * time.Second)
     }
 
     func checkErr(err error) {
@@ -28,11 +37,15 @@ package main
         }
     }
 
-    func getOrderAge(status string/*, ch chan int*/) {
-    	db, err := sql.Open("mysql", "root:huripdb@/newkalbe?charset=utf8")
+    func clear(db *sql.DB) {
+    	stmt, err := db.Prepare("TRUNCATE go_dashboard")
         checkErr(err)
 
-    	// query
+        stmt.Exec()
+    }
+
+    func getOrderAge(db *sql.DB, status string/*, ch chan int*/) {
+    	// query to get order age by pending date, based on order status
     	query := `select count(khdex_order_id) as total_orders, 
 datediff(date_pending, now()) as date_diff
 FROM khdex_order_go 
@@ -49,19 +62,37 @@ GROUP BY date_diff`
             var date_diff int
             err = rows.Scan(&total_orders, &date_diff)
             checkErr(err)
-            go insert(status, total_orders, (date_diff * -1))
-            //fmt.Printf("%d. pending : %d %d\n", i, total_orders, (date_diff * -1))
+            go insert(db, status, total_orders, (date_diff * -1))
+            //fmt.Printf("%d. %s : %d %d\n", i, status, total_orders, (date_diff * -1))
             i++
         }
-
-        //time.Sleep(2 * time.Second)
-
-        //close(ch)
     }
 
-    func insert(key string, total_orders int, date_diff int) {
-    	db, err := sql.Open("mysql", "root:huripdb@/newkalbe?charset=utf8")
+    func getPoAge(db *sql.DB, status string) {
+    	// query to get order age by pending date, based on order status
+    	query := `SELECT COUNT(stockist_po_id) as total_po, datediff(created_at, now()) as date_diff 
+FROM stockist_po WHERE status = '%s'
+GROUP BY date_diff`
+
+        rows, err := db.Query(fmt.Sprintf(query, status))
         checkErr(err)
+
+        i := 0
+        for rows.Next() {
+            var total_po int
+            var date_diff int
+            err = rows.Scan(&total_po, &date_diff)
+            checkErr(err)
+            dashboard_key := fmt.Sprintf(`po_%s`, status)
+            go insert(db, dashboard_key, total_po, (date_diff * -1))
+            //fmt.Printf("%d. %s : %d %d\n", i, dashboard_key, total_po, (date_diff * -1))
+            i++
+        }
+    }
+
+    func insert(db *sql.DB, key string, total_orders int, date_diff int) {
+    	//db, err := sql.Open("mysql", "root:huripdb@/newkalbe?charset=utf8")
+        //checkErr(err)
 
         // insert
         stmt, err := db.Prepare("INSERT go_dashboard SET go_dashboard_key=?,value_1=?,value_2=?,created_at=?")
@@ -69,6 +100,4 @@ GROUP BY date_diff`
 
         created_at := time.Now()
         stmt.Exec(key, total_orders, date_diff, created_at)
-
-        //time.Sleep(2 * time.Second)
     }
